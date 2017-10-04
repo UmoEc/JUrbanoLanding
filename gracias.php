445cdfb3f574e39c6ee4de2b812cd4ca3d4fb034
elCanarioVidal <pablo@umo.uy>
2017-10-04 02:15:39 -0300

    Proyecto completo
    Fin proyecto,
    Elementos de conversión y analytics añadidos

--- gracias.php
+++ gracias.php
@@ -15,6 +15,45 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <!-- Global Site Tag (gtag.js) - Google Analytics -->
     <link href="css/style.css" rel="stylesheet" type="text/css">
+
+<!-- Global Site Tag (gtag.js) - Google Analytics -->
+
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-19700721-2"></script>
+
+	<script>
+
+	  window.dataLayer = window.dataLayer || [];
+
+	  function gtag(){dataLayer.push(arguments)};
+
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-19700721-2');
+
+	</script> 
+<!-- End Google Analytics -->    
+
+<!-- Facebook Pixel Code -->
+	<script>
+	!function(f,b,e,v,n,t,s)
+	{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+	n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+	if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+	n.queue=[];t=b.createElement(e);t.async=!0;
+	t.src=v;s=b.getElementsByTagName(e)[0];
+	s.parentNode.insertBefore(t,s)}(window,document,'script',
+	'https://connect.facebook.net/en_US/fbevents.js');
+	 fbq('init', '139900230079902'); 
+	fbq('track', 'PageView');
+	fbq('track', 'CompleteRegistration');
+	</script>
+	<noscript>
+	 <img height="1" width="1" 
+	src="https://www.facebook.com/tr?id=139900230079902&ev=PageView
+	&noscript=1"/>
+	</noscript>
+<!-- End Facebook Pixel Code -->
+
   </head>
   <body>
 
