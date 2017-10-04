0aaac0914cfc841209c3c7278df3cd62f70a29ad
elCanarioVidal <pablo@umo.uy>
2017-10-04 02:07:00 -0300

    faltante
    faltante conmit anterior

--- gracias.php
+++ gracias.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Jardín Urbano</title>
     <link href="css/reset.css" rel="stylesheet" type="text/css">
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
 	<link rel="stylesheet" href="https://cdn.linearicons.com/free/1.0.0/icon-font.min.css">
     <!-- Bootstrap CSS -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
@@ -24,98 +24,153 @@
 	<div class="row">
 		<div class="col-md-12">
 			<div class="row cabecera">
-				<div class="col-md-6 dato-curso">
+				<div class="col-md-6 dato-curso-gracias">
 					<h1>
 						Taller de huerta<br>Orgánica
 					</h1>
 					<h2>
-						Inicio:<br>Octubre 2017
+						Inicio:<br>Noviembre 2017
 					</h2>
 				</div>
-				<div class="col-md-6 form-panel">
+				<!-- <div class="col-md-6 form-panel">
 				<div class="row">
 					<div class="col-md-10 col-md-offset-1">
-						<h2>REGISTRATE Y DESCARGÁ<br>EL PROGRAMA DEL CURSO</h2>
-					<form role="form" onsubmit="return formulario()" action="php/enviarmails.php" method="post">
-						<div class="form-group">
-							<label for="nombre">
-								Nombre
-							</label>
-							  <input type="text" class="form-control" id="nombre" placeholder="&#xe82a; nombre completo">
-						</div>
-						<div class="form-group">
-							<label for="Email1">
-								Email address
-							</label>
-							<input type="email" class="form-control" id="Email1" placeholder="&#xe818; email">
-						</div>
-							<label for="celular">
-								Email address
-							</label>
-							<input type="text" class="form-control" id="celular" placeholder="&#xe83a; celular">
-						<button type="submit" class="btn btn-default">
-							Enviar
-						</button>
-					</form>
+						<h2><strong>¡GRACIAS!</strong><br><br>POR REGISTRARTE A UN TALLER<br>DE JARDÍN URBANO</h2>
+						<p style="color: #fff;">Más abajo vas encontrar info que te va a ser muuy útil</p>
 					</div>
 				</div>
-				</div>
+				</div> -->
 			</div>
 		</div>
 	</div>
 	</div>
 	</header>
 	<content>
-	<div class="container">
-	<div class="row">
-		<div class="col-md-12">
-			<h1 class="text-center tit-obj">
-				objetivos
-			</h1>
-			<div class="row">
-				<div class="col-md-1">
-				</div>
-				<div class="col-md-2 text-center">
-					<div><img src="img/pala-icon.png" class="ico-cont"></div>
-					<h4>Conocimiento</h4>
-					<p class="text-center">
-						Donde, cómo y qué plantar para tener éxito
-					</p>
-				</div>
-				<div class="col-md-2 text-center">
-					<div><img src="img/loveeco-icon.png" class="ico-cont"></div>
-					<h4>Placer</h4>
-					<p class="text-center">
-						de Sembrar, plantar y cuidar
-					</p>
-				</div>
-				<div class="col-md-2 text-center">
-					<div><img src="img/cajon-icon.png" class="ico-cont"></div>
-					<h4>Saber</h4>
-					<p class="text-center">
-						Aprovechar cualquier espacio para plantar áromaticas o verduras
-					</p>
-				</div>
-				<div class="col-md-2 text-center">
-					<div><img src="img/eco-icon.png" class="ico-cont"></div>
-					<h4>Cudiar</h4>
-					<p class="text-center">
-						Cuidar tu huerta de forma ecológica para una alimentación sana
-					</p>
-				</div>
-				<div class="col-md-2 text-center">
-					<div><img src="img/flor-icon.png" class="ico-cont"></div>
-					<h4>Decorar</h4>
-					<p class="text-center">
-						Espacios de tu hogar asociando plantas
-					</p>
-				</div>
-				</div>
-				<div class="col-md-1">
+	<section class="formato">
+			<div class="container">
+				<h2>Temario <strong>del taller</strong></h2>
+				<div class="puntos">
+					<div class="col-lg-2">
+						<img src="http://www.jardinurbano.com.uy/wp-content/themes/123garden_child_theme/templates/imgs/1-planificacion.png" class="img-responsive">
+						<p>Planificación de la huerta.</p>
+					</div>
+					<div class="col-lg-2">
+						<img src="http://www.jardinurbano.com.uy/wp-content/themes/123garden_child_theme/templates/imgs/2-macetas.png" class="img-responsive">
+						<p>Contenedores y macetas.</p>
+					</div>
+					<div class="col-lg-2">
+						<img src="http://www.jardinurbano.com.uy/wp-content/themes/123garden_child_theme/templates/imgs/3-suelo.png" class="img-responsive">
+						<p>Suelo y sustrato.</p>
+					</div>
+					<div class="col-lg-2">
+						<img src="http://www.jardinurbano.com.uy/wp-content/themes/123garden_child_theme/templates/imgs/4-cultivos.png" class="img-responsive">
+						<p>Cultivos de invierno y de verano.</p>
+					</div>
+					<div class="col-lg-2">
+						<img src="http://www.jardinurbano.com.uy/wp-content/themes/123garden_child_theme/templates/imgs/5-semillas.png" class="img-responsive">
+						<p>Donde conseguir, las semillas, la tierra y demás insumos.</p>
+					</div>
+					<div class="col-lg-2">
+						<img src="http://www.jardinurbano.com.uy/wp-content/themes/123garden_child_theme/templates/imgs/6-otras-plantas.png" class="img-responsive">
+						<p>Asociación con aromáticas y otras plantas.</p>
+					</div>
+					<div class="col-lg-2 col-lg-offset-2">
+						<img src="http://www.jardinurbano.com.uy/wp-content/themes/123garden_child_theme/templates/imgs/7-calendario.png" class="img-responsive">
+						<p>Manejo del calendario de siembra y cosecha.</p>
+					</div>
+					<div class="col-lg-2">
+						<img src="http://www.jardinurbano.com.uy/wp-content/themes/123garden_child_theme/templates/imgs/8-mantenimiento.png" class="img-responsive">
+						<p>Instalación y mantenimiento de la huerta.</p>
+					</div>
+					<div class="col-lg-2">
+						<img src="http://www.jardinurbano.com.uy/wp-content/themes/123garden_child_theme/templates/imgs/9-hortalizas.png" class="img-responsive">
+						<p>Cosecha y conservación de algunas hortalizas</p>
+					</div>
+					<div class="col-lg-2">
+						<img src="http://www.jardinurbano.com.uy/wp-content/themes/123garden_child_theme/templates/imgs/10-sanidad.png" class="img-responsive">
+						<p>La sanidad de la huerta orgánica.</p>
+					</div>
 				</div>
 			</div>
-		</div>
-		</div>
+		</section>	
+		</section>
+				<section class="info">
+			<div class="container">
+				<h2>Información sobre<br> el taller</h2>
+			</div>
+		</section>
+		<section class="info-curso">
+			<div class="container">
+				<div class=" col-md-12 table-responsive">
+					<table class="table table-curso">
+					<tr class="titulos">
+							<div class="barramvd">Montevideo</div>
+						</tr>
+
+						<tr class="titulos">
+							<td>Días</td>
+							<td>Horario</td>
+							<td>Comienzo</td>
+							<td>Fin</td>
+							<td>Lugar</td>
+						</tr>
+						<tr>
+							<td>Lunes</td>
+							<td>18.30 a 20.30</td>
+							<td>06/11</td>
+							<td>27/11</td>
+							<td>Llambí 1410 esquina Rivera,<br><span class="verde">Parque Batlle</span>.</td>
+						</tr>
+						<tr>
+							<td>Miércoles</td>
+							<td>14:30 a 16:30</td>
+							<td>08/11</td> 
+							<td>29/11</td>
+							<td>Llambí 1410 esquina Rivera,<br><span class="verde">Parque Batlle</span>.</td>
+						</tr>
+						 <tr>
+							<td>Sábado</td>
+							<td>10.30 a 12.30</td>
+							<td>11/11</td>
+							<td>02/12</td>
+							<td>Llambí 1410 esquina Rivera,<br><span class="verde">Parque Batlle</span>.</td>
+						</tr> 					
+					</table>
+				</div>
+				<p><small>*Cada clase dura aproximadamente 2 horas / Frecuencia 1 vez por semana / Seleccionar 1 día aunque puede cambiar semana a semana / Total 4 clases.</small></p>
+			</section>	
+			<section class="precios">
+			<div class="container">
+				<div class="col-md-12">
+					<h4>Precios</h4>
+				</div>
+				<div class="col-lg-6">
+					<table class="medios">
+						<tr>
+							<td>Contado</td>
+							<td><span class="contado"><small>$</small> 3900</span></td>
+						</tr>
+						<tr>
+							<td>OCA o VISA<br><span class="cuotas">hasta en 6 pagos</span></td>
+							<td><span class="cuotas-precio"><small>$</small> 4700</span></td>
+						</tr>
+					</table>
+					<p style="display: table;padding: 5px;">*Reservá tu lugar en el taller con una seña de $1000</p>
+				</div>
+				<div class="col-lg-6">
+					<p class="descuento">20%<br><span>DE DESCUENTO</span></p>
+					<p class="info-desc">para quien se anote en grupo (mínimo 2 personas).</p>
+				</div>
+			</div>
+		</section>
+		<section class="contacto">
+			<div class="container">
+				<h2>Por Consultas e Inscripciones</h2>
+				<p>Comunicarse con el Prof. Pablo Machado al celular:<br> 
+					<strong>094 387 928</strong> llamando o  por sms o Whatsapp o al <br>
+					mail <strong><small>jardinurbano@montevideo.com.uy</small></strong></p>
+			</div>
+		</section>
 	</content>
 	<footer>
 		<div class="container">
@@ -125,9 +180,9 @@
 					<div class="col-md-4 redes">
 						<h3>SOCIAL MEDIA</h3>
 						<div class="col-md-8">
-							<div class="rounded social"><a href="#">&#xf09a;</a></div>
-							<div class="rounded social"><a href="#">&#xf167;</a></div>
-							<div class="rounded social"><a href="#">&#xf231;</a></div>
+							<div class="rounded social"><a href="https://www.facebook.com/jardinurbano" target="blank">&#xf09a;</a></div>
+							<div class="rounded social"><a href="https://www.youtube.com/channel/UCWZHKmzm-BlaUlb3yrnQbJg" target="blank">&#xf167;</a></div>
+							<div class="rounded social"><a href="https://es.pinterest.com/jardinurbanomde/" target="blank">&#xf231;</a></div>
 						</div>
 					</div>
 					<div class="col-md-4 logo">
@@ -135,10 +190,10 @@
 							<img src="img/logo.png" class="rounded center-block">
 						</div>
 					</div>
-					<div class="col-md-4 info">
+					<div class="col-md-4 infopie">
 						<p class="text-right">
 							Tel.: 094381298<br>
-							info@jardinurbano.com.uy<br>
+							<a href="mailto:info@jardinurbano.com.uy?Subjet=Consulta%20web">info@jardinurbano.com.uy</a><br>
 							Prof. Pablo Machado
 						</p>
 					</div>
@@ -149,8 +204,7 @@
 	</footer>
 	<div class="pie">
 		<div class="container">
-			<div class="row">
-			</div>
+			<div class="row"></div>
 		</div>
 	</fdiv>
 </div>
